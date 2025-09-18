15dd543c86bc7a8f758f4a4756bd46f521212404
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-09-18 15:22:06 -0300

    Initial Commit

---
+++ BudgetBuddy/BudgetBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddyApp.swift
+//  BudgetBuddy
+//
+//  Created by Francisco Manuel Gallegos Luque on 18/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetBuddyTests/BudgetBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddyTests.swift
+//  BudgetBuddyTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 18/09/2025.
+//
+
+import Testing
+@testable import BudgetBuddy
+
+struct BudgetBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
